7bf2f4e21bbe53279487ffe7aa6d4db4b3d14d95
Ger Teck <changerteck@gmail.com>
2024-07-09 17:04:08 +0800

    Add Ex 2.7/8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,44 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '9889 8998'
+     }
   ]) 
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (newName === '') {
+    if (newName === '' || newNumber === '') {
+      alert(`Input fields cannot be empty`);
+      return;
+    }
+
+    // newNumber can only have digits and dashes
+    if (!newNumber.match(/^[0-9-]+$/)) {
+      alert(`Number can only have digits and dashes`);
+      return;
+    }
+
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    if (persons.some(person => person.number === newNumber)) {
+      alert(`${newNumber} is already added to phonebook`);
       return;
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
 
     setPersons(persons.concat(personObject));
     setNewName('');
+    setNewNumber('');
   };
 
   return (
@@ -28,11 +50,14 @@ const App = () => {
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
