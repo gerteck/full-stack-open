f7cf75e12031aa5a20e1a802a027240241ce15da
Ger Teck <changerteck@gmail.com>
2024-07-22 11:45:32 +0800

    Add Ex5.1
    Add login functionality to Blog List Frontend

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,17 +1,88 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
+
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs => setBlogs( blogs ));  
-  }, [])
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const userDetails = () => (
+    <div>
+      <p>{user.name} logged-in!</p>
+      <button onClick={() => setUser(null)}>logout</button>
+    </div>
+  );
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <Notification message={errorMessage} isError />
+        {loginForm()}
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} isError />
+
+      {userDetails()}
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
